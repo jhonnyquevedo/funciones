e1919af5ca134b781a52fea44bdf9fc0f46e4754
jhonnyquevedo <142636344+jhonnyquevedo@users.noreply.github.com>
2023-11-04 19:23:32 -0300

    Add files via upload

---
+++ desafio_4_funciones/assets/js/script.js
@@ -0,0 +1,24 @@
+const ele1 = document.getElementById("div-azul")
+const ele2 = document.getElementById("div-rojo")
+const ele3 = document.getElementById("div-verde")
+const ele4 = document.getElementById("div-amarillo")
+
+ele1.addEventListener("click", function(){
+    ele1.style.backgroundColor = "black";
+})
+
+ele2.addEventListener("click", function(){
+    ele2.style.backgroundColor = "black";
+})
+
+ele3.addEventListener("click", function(){
+    ele3.style.backgroundColor = "black";
+})
+
+ele4.addEventListener("click", function(){
+    ele4.style.backgroundColor = "black";
+})
+
+
+
+ 
